f5ab9b641e6b7c1fcf7b81bf7110bdbfb1bf1332
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-08-19 12:35:52 +0200

    Camera preview

--- Screens/CameraScreen.js
+++ Screens/CameraScreen.js
@@ -1,6 +1,35 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Camera } from "expo-camera";
 
 export default function CameraScreen() {
-  return <Text>Camera Screen</Text>;
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+});
